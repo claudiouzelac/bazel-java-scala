e1958b03d7cd69fd03596d94a5062462be6bbf63
Stewart Henderson <henderson.geoffrey@gmail.com>
2016-08-31 17:11:16 -0500

    added bazel aliases

---
+++ scripts/bazel-alias.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# NOTE: This script contains Bazel alias that you can use by
+# `source bazel-alias.sh`
+
+alias bzb="bazel build //..."
+alias bzt="bazel test //..."
+alias bzr="bazel run //:ScalaMain"
